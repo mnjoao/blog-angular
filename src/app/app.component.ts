5cf36a2dae2d25a3d4fd7611e4b167cef857bd99
João Marcos de Morais <mnjoao3@gmail.com>
2023-12-08 15:07:14 -0300

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./componentes/small-card/small-card.component";
+import { MenuTitleComponent } from "./componentes/menu-title/menu-title.component";
+import { BigCardComponent } from "./componentes/big-card/big-card.component";
+import { MenuBarComponent } from "./componentes/menu-bar/menu-bar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, SmallCardComponent, MenuTitleComponent, BigCardComponent, MenuBarComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
